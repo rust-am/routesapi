2dac937e17c149ee4f1b80729cd7ca06f15c5bc8
Rustam <r.91@mail.ru>
2023-05-10 17:35:28 +0300

    workflows deploy

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store,key: "_authentication_app", domain: "something-routes-app-api.heroku.com"
+  Rails.application.config.session_store :cookie_store,key: "_authentication_app" # TODO , domain: "something-routes-app-api.heroku.com"
 else
   Rails.application.config.session_store :cookie_store,key: "_authentication_app"
 end
